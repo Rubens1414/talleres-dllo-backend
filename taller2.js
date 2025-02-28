2954a9fe8b21db8e14871af99e69026d693fc615
Rubens1414 <98895114+Rubens1414@users.noreply.github.com>
2025-02-28 18:49:05 +0000

    Punto 4 se me habia olvidado jsjs

--- taller2.js
+++ taller2.js
@@ -30,7 +30,37 @@ function sum(lista) {
     }
     return suma;
 }
+
+//funcion min para Punto 4
+function min(lista) {
+  var numero_min = lista[0];
+  
+  for (var i = 1; i < lista.length; i++) { 
+    var numero_actual = lista[i];
+    if(numero_actual < numero_min) { 
+      numero_min = numero_actual
+    }
+  }
+  return numero_min;
+}
+//Punto 4 (Funcion missingNumbers)
+
+function missingNumbers(lista) {
+    var numeros_faltantes = [];
+    var maximo = findMax(lista);
+    var minimo = min(lista);
+ 
+    for (var i = minimo; i < maximo; i++) {
+      if (!includes(lista, i)) {
+        
+          numeros_faltantes.push(i);
+     
+      }
+    }
+    return numeros_faltantes;
+}
 console.log(findMax([3, 17,-1, 4, -19]));
 console.log(includes([3, 17,-1, 4, -19], 4));
 console.log(includes([3, 17,-1, 4, -19], 2));
 console.log(sum([3, 17,-1, 4, -19]));
+console.log(missingNumbers([7,2,4,6,3,9]));
